d14dd422a43dbe0eaf702f6fd812373dcde52a36
KekZW <147482043+KekZW@users.noreply.github.com>
2024-01-30 15:54:38 +0800

    Update Account.java

--- src/Account.java
+++ src/Account.java
@@ -4,6 +4,7 @@ public class Account {
     //This is using DBS bank account as references
     private float balance;
     private int accountcount;
+    private String accountname; //????
     private String userID;
     private String password; //Or it could be a pin number <-- this can be in hash
 
